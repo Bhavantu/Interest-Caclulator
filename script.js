2df9489db8543d6285a1dc3b83d458208b851c3c
Bhavantu <80723750+Bhavantu@users.noreply.github.com>
2022-07-27 16:28:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+/* Compute button code*/
+function compute()
+{
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = principal * years * rate /100;
+  var year = new Date().getFullYear()+parseInt(years);
+
+    if(principal == undefined || principal <=0){
+          alert("Enter a positive Number");
+          document.getElementById("principal").focus();
+          return
+      }
+      
+      else{
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark> \<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>\<br\> in the year <mark>"+year+"</mark>\<br\>"
+      }
+}
+
+/*Update Rate for the slider*/
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
+
